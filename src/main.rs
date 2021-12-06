e7ed2913d8f99eac8148b07f912b921645fb8c4e
yoshidan <naohiro.y@gmail.com>
2021-12-06 13:51:26 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ use nene::repository::TableRepository;
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let database: &str = "projects/local-project/instances/test-instance/databases/local-database";
-
-    std::env::set_var("SPANNER_EMULATOR_HOST", "localhost:9010");
-    //let database = std::env::var("SPANNER_DSN");
+    let database = std::env::var("SPANNER_DSN");
     let matches = App::new("Spanner ORM Generator")
         .arg(
             Arg::with_name("input_dir")
