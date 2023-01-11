921e5fd3eeb1e34fefa664f880e515bfae024276
yoshidan <naohiro.y@gmail.com>
2023-01-11 14:47:15 +0900

    change ci

--- tests/test.rs
+++ tests/test.rs
@@ -1,24 +1,28 @@
 mod gen;
 
-use std::env;
 use gen::user;
+use std::env;
 
 #[tokio::test]
 async fn test_generated() {
-    let v = user::User{
+    let v = user::User {
         user_id: "test_user".to_string(),
         ..Default::default()
     };
 
     let database = env::var("SPANNER_DSN").unwrap();
-    let client = google_cloud_spanner::client::Client::new(database).await.unwrap();
+    let client = google_cloud_spanner::client::Client::new(database)
+        .await
+        .unwrap();
     let _ = client.apply(vec![v.insert_or_update()]).await.unwrap();
     let mut tx = client.single().await.unwrap();
-    let user = user::User::find_by_pk(&mut tx, &"test_user", None).await.unwrap();
+    let user = user::User::find_by_pk(&mut tx, &"test_user", None)
+        .await
+        .unwrap();
     assert!(user.is_some());
 
     let ser = serde_json::to_string(&v).unwrap();
-    let dser : user::User = serde_json::from_str(ser.as_str()).unwrap();
+    let dser: user::User = serde_json::from_str(ser.as_str()).unwrap();
     assert_eq!(dser.user_id, v.user_id);
     assert_eq!(dser.updated_at, v.updated_at);
     assert_eq!(dser.not_null_date, v.not_null_date);
